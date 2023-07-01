897548b989bcfcfd1184d7716515bef6f14341e1
Prabhat Gupta <98599890+Prabhat-gupt@users.noreply.github.com>
2023-07-01 10:41:03 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -29,7 +29,7 @@ export const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
+    let response = await fetch("https://serverf-j7ks.onrender.com/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
